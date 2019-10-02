3e8292f40aca48691562e597fc5c7409d00e6e9f
oanacodes <oana.carlescu@gmail.com>
2019-10-02 10:57:26 +0200

    created get and post endpoints

--- image/router.js
+++ image/router.js
@@ -11,4 +11,10 @@ router.get('/images', (req, res, next) => {
       .catch(console.error)
 })
 
+router.post('/images', (req, res, next) => {
+  Image.create(req.body)
+    .then(image => res.json(image))
+    .catch(next)
+})
+
 module.exports = router
--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express')
 const db = require('./db')
 const Image = require('./image/model')
 const imageRouter = require('./image/router')
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 const port = 4005
 
-// app.use(bodyParser.json())
-
-// setup app port listening
 app.listen(port, () => {
   console.log(`I'm listening on port ${port} `)
 })
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
 
 app.use(imageRouter)
