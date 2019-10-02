388a76c13a451355ed21fbc8d2580968b9334d30
oanacodes <oana.carlescu@gmail.com>
2019-10-02 10:24:27 +0200

    initialized project, created gitignore

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+// const bodyParser = require('body-parser')
+
+const app = express()
+const port = 4005
+
+// app.use(bodyParser.json())
+
+// setup app port listening
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port} `)
+})
